031b31d2d8426f437830ec7bd4506ebdbb3753cd
moura1001 <gmouraneto01@gmail.com>
2021-12-27 11:25:21 -0300

    commit com movimentação vertical

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    private float previousTime;
+    private float fallTime = 0.8f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,12 @@ public class Tetromino : MonoBehaviour
         else if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             transform.position += new Vector3(1, 0, 0);
+        }
+
+        if(Time.time - previousTime > (Input.GetAxisRaw("Vertical") < 0f ? fallTime/10f : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            previousTime = Time.time;
         }
     }
 }
