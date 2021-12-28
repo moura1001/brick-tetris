3ef5457235ea808ff2a395215df08010ffa89a8d
moura1001 <gmouraneto01@gmail.com>
2021-12-28 11:18:02 -0300

    commit com validação da movimentação em relação ao grid

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -6,6 +6,9 @@ public class Tetromino : MonoBehaviour
 {
     private float previousTime;
     private float fallTime = 0.8f;
+
+    private static int width = 10;
+    private static int height = 20;
     
     // Start is called before the first frame update
     void Start()
@@ -19,17 +22,45 @@ public class Tetromino : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") < 0f)
         {
             transform.position += new Vector3(-1, 0, 0);
+            if (!IsValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
 
         }
         else if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             transform.position += new Vector3(1, 0, 0);
+            if (!IsValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
         }
 
         if(Time.time - previousTime > (Input.GetAxisRaw("Vertical") < 0f ? fallTime/10f : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!IsValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
             previousTime = Time.time;
         }
+    }
+
+    private bool IsValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
